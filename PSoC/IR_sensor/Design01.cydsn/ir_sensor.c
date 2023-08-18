4e5a60568d138f730ad0130c431f05e817aeaba2
jklim01 <jklim0155@gmail.com>
2023-08-19 02:44:37 +0800

    Added example for interfacing IR sensor

---
+++ PSoC/IR_sensor/Design01.cydsn/ir_sensor.c
@@ -0,0 +1,75 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include <stddef.h>
+
+#include "ir_sensor.h"
+#include "ir_posedge_isr.h"
+#include "ir_negedge_isr.h"
+
+
+// globals
+volatile bool ir_detected = false;
+
+
+// static globals
+void (*ir_posedge_user_handler)(void) = NULL;
+void (*ir_negedge_user_handler)(void) = NULL;
+
+
+// ISRs
+CY_ISR_PROTO(ir_posedge_handler) {
+    ir_detected = false;
+}
+
+CY_ISR_PROTO(ir_negedge_handler) {
+    ir_detected = true;
+}
+
+CY_ISR_PROTO(ir_posedge_handler_extended) {
+    ir_posedge_handler();
+    (*ir_posedge_user_handler)();
+}
+
+CY_ISR_PROTO(ir_negedge_handler_extended) {
+    ir_negedge_handler();
+    (*ir_negedge_user_handler)();
+}
+
+
+// API
+void setup_ir_sensor(void (*ir_detected_posedge_handler)(void), void (*ir_detected_negedge_handler)(void)) {
+    if (ir_detected_posedge_handler == NULL)
+        ir_negedge_isr_StartEx(ir_negedge_handler);
+    else {
+        ir_negedge_isr_StartEx(ir_negedge_handler_extended);
+        ir_negedge_user_handler = ir_detected_posedge_handler;
+    }
+
+    if (ir_detected_negedge_handler == NULL)
+        ir_posedge_isr_StartEx(ir_posedge_handler);
+    else {
+        ir_posedge_isr_StartEx(ir_posedge_handler_extended);
+        ir_posedge_user_handler = ir_detected_negedge_handler;
+    }
+}
+
+void pause_ir_sensor(void) {
+    ir_negedge_isr_Stop();
+    ir_posedge_isr_Stop();
+}
+
+void resume_ir_sensor(void) {
+    setup_ir_sensor(ir_negedge_user_handler, ir_posedge_user_handler);
+}
+
+/* [] END OF FILE */
---
+++ PSoC/IR_sensor/Design01.cydsn/ir_sensor.h
@@ -0,0 +1,38 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef IR_SENSOR_H
+#define IR_SENSOR_H
+    
+#include <stdbool.h>
+#include "cytypes.h"
+
+
+// globals
+extern volatile bool ir_detected;
+
+
+// ISRs
+CY_ISR_PROTO(ir_posedge_handler);
+CY_ISR_PROTO(ir_negedge_handler);
+CY_ISR_PROTO(ir_posedge_handler_extended);
+CY_ISR_PROTO(ir_negedge_handler_extended);
+
+// API
+void setup_ir_sensor(void (*ir_detected_posedge_handler)(void), void (*ir_detected_negedge_handler)(void));
+void pause_ir_sensor(void);
+void resume_ir_sensor(void);
+
+    
+#endif
+
+/* [] END OF FILE */
